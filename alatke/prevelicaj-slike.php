d147ab6fc44e6ea30e08f1c8c8549a7047dc1dc5
Damjan <dpavlica@itekako.com>
2016-07-05 15:42:11 +0200

    m

--- alatke/prevelicaj-slike.php
+++ alatke/prevelicaj-slike.php
@@ -9,7 +9,7 @@ foreach (glob($ulazniFolder."/*.*") as $filePath) {
     $ext = pathinfo($filePath, PATHINFO_EXTENSION);
     $ext = ".".$ext;  // dodaje tacku ekstenziji
     $newPath = str_replace($ulazniFolder, $izlazniFolder, $filePath);
-    $newPath = str_replace($ext, "-200".$ext, $newPath); // pravi novo ime
+    $newPath = str_replace($ext, "-200px".$ext, $newPath); // pravi novo ime
 
     echo $filePath . "\r\n";
     echo $newPath . "\r\n";
