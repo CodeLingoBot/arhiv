52ea25fa3c11e5999420874cc935d7d82d8addf7
mudroljub <mudroljub@gmail.com>
2016-07-08 23:54:58 +0200

    meni item

--- ukljuci/zaglavlje.php
+++ ukljuci/zaglavlje.php
@@ -7,8 +7,7 @@
 	$naslov = $naslov ?: "Baza dokumenata o Velikom oslobodilačkom ratu";
 
 	$ulogovan = false;
-	if($_SESSION["nadimak"] == "gost" || $_COOKIE["nadimak"] == "gost")
-	{
+	if($_SESSION["nadimak"] == "gost" || $_COOKIE["nadimak"] == "gost") {
 		$ulogovan = true;
 	}
 
@@ -37,11 +36,11 @@
 			<a href="<?php echo BASE_URL; ?>index.php"><li <?php
 			if ($tekuca_strana == "index.php" || $tekuca_strana == "" ) {
 				echo "class='tekuca_strana'";} ;?>>Na današnji dan</li></a>
-			<a href="<?php echo BASE_URL; ?>biblioteka.php"><li>Biblioteka</li></a>
-			<a href="<?php echo BASE_URL; ?>pretraga.php"><li>Pretraga</li></a>
-			<a href="<?php echo BASE_URL; ?>pojmovi.php"><li>Svi pojmovi</li></a>
-			<a href="<?php echo BASE_URL; ?>fotogalerija.php"><li>Fototeka</li></a>
-			<a href="<?php echo BASE_URL; ?>prijava.php"><li>Administracija</li></a>
+			<a href="<?php echo BASE_URL; ?>biblioteka.php"><li class="meni-item">Biblioteka</li></a>
+			<a href="<?php echo BASE_URL; ?>pretraga.php"><li class="meni-item">Pretraga</li></a>
+			<a href="<?php echo BASE_URL; ?>pojmovi.php"><li class="meni-item">Svi pojmovi</li></a>
+			<a href="<?php echo BASE_URL; ?>fotogalerija.php"><li class="meni-item">Fototeka</li></a>
+			<a href="<?php echo BASE_URL; ?>prijava.php"><li class="meni-item">Administracija</li></a>
 		</ul>
 		<div class="clear"></div>
 		<div class="krasnopis">Baza podataka o drugom svetskom ratu na tlu Jugoslavije</div>
