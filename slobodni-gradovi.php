7d14f716b11f51f148d16a03b7aad65eb3f7f52d
Damjan <dpavlica@itekako.com>
2016-07-08 09:04:25 +0200

    gradovi (5)

--- slobodni-gradovi.php
+++ slobodni-gradovi.php
@@ -75,9 +75,8 @@ if ($rezultat_za_gradove = $mysqli->query($upit_za_gradove)) {
     </style>
 
     <script>
-	   var gradovi = <?php echo json_encode($gradovi); ?>;
      window.onload = function() {
-       postaviMapu(gradovi);
+       postaviMapu(<?php echo json_encode($gradovi); ?>);
      }
     </script>
 </head>
