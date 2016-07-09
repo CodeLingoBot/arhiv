b4f4562c3a2f62a67535da90b92ee6e1e70aab4f
mudroljub <mudroljub@gmail.com>
2016-07-10 00:49:41 +0200

    kolone

--- pojam.php
+++ pojam.php
@@ -65,14 +65,14 @@ include_once(ROOT_PATH . 'ukljuci/zaglavlje.php');
 			</div>
 		</div>
 
-		<div class="fotografije" onscroll="ucitajJos(this)">
+		<div class="kolone fotografije" onscroll="ucitajJos(this)">
 			<h2 class="naslov-odeljka">Fotografije </h2>
 			<div id="fotografije">
 				<p class="ucitavac"><img src="slike/ajax-loader.gif" alt="loading" /> Istorijske fotografije se učitavaju...</p>
 			</div>
 		</div>
 
-		<div class="tagovi">
+		<div class="kolone tagovi">
 			<h2 class="naslov-odeljka">Povezani pojmovi: </h2>
 			<div id="tagovi">
 				<p><img src="slike/ajax-loader.gif" alt="loading" /> Povezani pojmovi se generišu...</p>
