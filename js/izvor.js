f7db826caddd3c6d0ae052de6594bbe423d1b7d3
mudroljub <mudroljub@gmail.com>
2016-07-10 11:20:26 +0200

    separate izvor.js (4)

--- js/izvor.js
+++ js/izvor.js
@@ -4,6 +4,7 @@ platno.height = window.innerHeight;
 
 var sadrzaj = platno.getContext('2d');
 sadrzaj.font = "bold 16px Arial";
+sadrzaj.fillText("Dokument se učitava...", platno.width/2-100, 100);
 
 var datum_prikaz = document.getElementById('datum-prikaz');
 if (datum_prikaz.innerText == "0000-00-00.") datum_prikaz.innerText = " nepoznat";
