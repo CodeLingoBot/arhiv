d7831d61fd8030735328db107ec8d9e3f320b405
mudroljub <mudroljub@gmail.com>
2016-07-10 15:38:21 +0200

    $

--- js/izvor.js
+++ js/izvor.js
@@ -1,4 +1,4 @@
-var platno = document.getElementById('platno');
+var platno = $('#platno');
 platno.width = platno.parentElement.offsetWidth;
 platno.height = window.innerHeight;
 
@@ -6,17 +6,17 @@ var sadrzaj = platno.getContext('2d');
 sadrzaj.font = "bold 16px Arial";
 sadrzaj.fillText("Dokument se učitava...", platno.width/2-100, 100);
 
-var datum_prikaz = document.getElementById('datum-prikaz');
+var datum_prikaz = $('#datum-prikaz');
 if (datum_prikaz.innerText == "0000-00-00.") datum_prikaz.innerText = " nepoznat";
 
-var opis = document.getElementById('opis');
+var opis = $('#opis');
 opis.contentEditable = true;
-var novi_opis = document.getElementById('novi_opis');
+var novi_opis = $('#novi_opis');
 
 function promeniOpis(id, vrsta){
     novi_opis.value = opis.textContent || opis.innerText;
 }
 
 function isprazniPolje(){
-    document.getElementById('tag').value = "";
+    $('#tag').value = "";
 }
