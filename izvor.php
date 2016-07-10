f04a4553bb60791bbd9b888a9c9f5f900b24dc81
mudroljub <mudroljub@gmail.com>
2016-07-10 15:47:40 +0200

    input hidden

--- izvor.php
+++ izvor.php
@@ -148,58 +148,10 @@ var id = <?php echo $id; ?>;
 
     </div>
 
+<input type="hidden" id="fajl_url" value="<?php echo $ova_datoteka->relativ_url; ?>">
+<input type="hidden" id="brojStrane" value="<?php echo $ova_datoteka->broj_strane; ?>">
+
 <script defer src='js/libs/pdf.js'></script>
 <script defer src="js/izvor.js"></script>
-
-<?php if($vrsta == 2) { // samo za dokumente ide pdf.js ?>
-<script>
-
-var fajl_url = '<?php echo $ova_datoteka->relativ_url; ?>';
-var brojStrane = <?php echo $ova_datoteka->broj_strane; ?>;
-
-// disable workers to avoid cross-origin issue
-PDFJS.disableWorker = true;
-var ovajDokument = null;
-
-function renderujStranu(broj) {
-    // koristi promise da fetchuje stranu
-    ovajDokument.getPage(broj).then(function(strana) {
-        // proporcionalno prilagodjava raspoloživoj širini
-        var roditeljskaSirina = platno.parentElement.offsetWidth;
-        var viewport = strana.getViewport( roditeljskaSirina / strana.getViewport(1.0).width );
-        platno.height = viewport.height;
-        platno.width = viewport.width;
-        // renderuje PDF stranu u sadrzaj platna
-        var renderContext = {
-            canvasContext: sadrzaj,
-            viewport: viewport
-        };
-        strana.render(renderContext);
-    });
-    document.getElementById('trenutna_strana').textContent = brojStrane;
-    document.getElementById('ukupno_strana').textContent = ovajDokument.numPages;
-}
-
-function idiNazad() {
-    if (brojStrane <= 1) return;
-    brojStrane--;
-    renderujStranu(brojStrane);
-}
-
-function idiNapred() {
-    if (brojStrane >= ovajDokument.numPages) return;
-    brojStrane++;
-    renderujStranu(brojStrane);
-}
-
-// asinhrono downloaduje PDF kao ArrayBuffer
-PDFJS.getDocument(fajl_url).then(function getPdfHelloWorld(_pdfDoc) {
-    ovajDokument = _pdfDoc;
-    if(brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages;
-    renderujStranu(brojStrane);
-});
-</script>
-<?php } // kraj pdfa ?>
-
 
 <?php include_once(ROOT_PATH . "ukljuci/podnozje.php"); ?>
--- js/izvor.js
+++ js/izvor.js
@@ -20,3 +20,50 @@ function promeniOpis(id, vrsta){
 function isprazniPolje(){
     $('#tag').value = "";
 }
+
+
+// if($vrsta == 2), samo za dokumente treba pdf.js
+var fajl_url = $('#fajl_url').value;
+var brojStrane = $('#brojStrane').value;
+
+// disable workers to avoid cross-origin issue
+PDFJS.disableWorker = true;
+var ovajDokument = null;
+
+function renderujStranu(broj) {
+    // koristi promise da fetchuje stranu
+    ovajDokument.getPage(broj).then(function(strana) {
+        // proporcionalno prilagodjava raspoloživoj širini
+        var roditeljskaSirina = platno.parentElement.offsetWidth;
+        var viewport = strana.getViewport( roditeljskaSirina / strana.getViewport(1.0).width );
+        platno.height = viewport.height;
+        platno.width = viewport.width;
+        // renderuje PDF stranu u sadrzaj platna
+        var renderContext = {
+            canvasContext: sadrzaj,
+            viewport: viewport
+        };
+        strana.render(renderContext);
+    });
+    document.getElementById('trenutna_strana').textContent = brojStrane;
+    document.getElementById('ukupno_strana').textContent = ovajDokument.numPages;
+}
+
+function idiNazad() {
+    if (brojStrane <= 1) return;
+    brojStrane--;
+    renderujStranu(brojStrane);
+}
+
+function idiNapred() {
+    if (brojStrane >= ovajDokument.numPages) return;
+    brojStrane++;
+    renderujStranu(brojStrane);
+}
+
+// asinhrono downloaduje PDF kao ArrayBuffer
+PDFJS.getDocument(fajl_url).then(function getPdfHelloWorld(_pdfDoc) {
+    ovajDokument = _pdfDoc;
+    if(brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages;
+    renderujStranu(brojStrane);
+});
