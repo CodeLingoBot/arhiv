fe542e3eacd7f54cf0cc8f69a23fa09841939828
mudroljub <mudroljub@gmail.com>
2016-07-11 14:11:53 +0200

    proba

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Proba</title>
+</head>
+<body>
+
+  <?php
+
+    echo "Zdravo svima!";
+
+    include "ukljuci/header.php";
+
+   ?>
+
+</body>
+</html>
