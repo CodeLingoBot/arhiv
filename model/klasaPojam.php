9f2f22e04a294824d8ab369ea9a463610f661758
mudroljub <mudroljub@gmail.com>
2016-07-11 21:52:02 +0200

    m

--- model/klasaPojam.php
+++ model/klasaPojam.php
@@ -4,7 +4,7 @@
   Na osnovu id-a dobavlja sve povezane materijale o pojmu.
 */
 
-include "../ukljuci/povezivanje2.php";
+include_once "../ukljuci/povezivanje2.php";
 include_once "klasaIzvor.php";
 
 class Oznaka {
