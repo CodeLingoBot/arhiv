2d8cd029d5f7f6541341bd1ef856fbd2ba076809
Damjan <dpavlica@itekako.com>
2016-07-12 13:53:12 +0200

    dodat kesh na index

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+/*** kesh ***/
+$cache_ext  = '.html'; // file extension
+$cache_time     = 3600;  // 1 hour = 3600 sec
+$cache_folder   = '.kesh/';
+$dynamic_url    = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING']; // requested dynamic page (full url)
+$cache_file     = $cache_folder.md5($dynamic_url).$cache_ext; // construct a cache file
+if (file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
+    ob_start('ob_gzhandler'); //Turn on output buffering, "ob_gzhandler" for the compressed page with gzip.
+    readfile($cache_file); //read Cache file
+    echo '<!-- cached page - '.date('l jS \of F Y h:i:s A', filemtime($cache_file)).', Page : '.$dynamic_url.' -->';
+    ob_end_flush(); //Flush and turn off output buffering
+    exit(); //no need to proceed further, exit the flow.
+}
+ob_start('ob_gzhandler'); // start output buffering with gzip compression.
+/*** kesh ***/
 
 $naslov = "Na današnji dan";
 require_once("ukljuci/config.php");
@@ -187,4 +202,16 @@ $svi_tagovi = array();
 
     </div>
 
-<?php include_once(ROOT_PATH . "ukljuci/podnozje.php"); ?>
+<?php
+  include_once(ROOT_PATH . "ukljuci/podnozje.php");
+
+  /*** kesh ***/
+  if (!is_dir($cache_folder)) {
+      mkdir($cache_folder);
+  }
+  $fp = fopen($cache_file, 'w');  //open file for writing
+  fwrite($fp, ob_get_contents()); //write contents of the output buffer in Cache file
+  fclose($fp); //Close file pointer
+  ob_end_flush(); //Flush and turn off output buffering
+  /*** kesh ***/
+?>
