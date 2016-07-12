a228544dd34628f2e672e5e0b7d5332091f52c70
Damjan <dpavlica@itekako.com>
2016-07-12 18:53:28 +0200

    m

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $cache_file     = $cache_folder.md5($dynamic_url).$cache_ext; // construct a cac
 if (file_exists($cache_file) && time() - $cache_time < filemtime($cache_file)) { //check Cache exist and it's not expired.
     ob_start('ob_gzhandler'); //Turn on output buffering, "ob_gzhandler" for the compressed page with gzip.
     readfile($cache_file); //read Cache file
-    echo '<!-- cached page - '.date('l jS \of F Y h:i:s A', filemtime($cache_file)).', Page : '.$dynamic_url.' -->';
+    echo '<!-- keširano - '.date('d-m-Y \u H:i:s', filemtime($cache_file)).', stranica: '.$dynamic_url.' -->';
     ob_end_flush(); //Flush and turn off output buffering
     exit(); //no need to proceed further, exit the flow.
 }
