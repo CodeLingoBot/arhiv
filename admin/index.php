1cbdac01b26e1cbeb9b6c159454dbdb7e7cefc1c
Damjan <dpavlica@itekako.com>
2016-07-15 15:30:46 +0200

    logovanje (2)

---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once("ukljuci/config.php");
+    include_once(ROOT_PATH . 'ukljuci/zaglavlje.php');
+
+?>
+
+    <div class="okvir">
+
+        <h1 id="naslov-pretraga">Administracija</h1>
+
+        <?php
+        if (!$ulogovan) {
+            echo "<p>Morate biti <a href='prijava.php' class='underline'>prijavljeni</a> da biste pristupili administriranju.</p>";
+            die();
+        }
+        ?>
+
+        <p>Prijavljen si i imaš pristup sledećim opcijama: </p>
+
+        <ul>
+            <li><a href="admin/provera.php">Pretraži i ažuriraj dokumente</a></li>
+            <li><a href="admin/taguje-sve.php">Masovno taguj</a></li>
+            <li><a href="admin/unesi-zapis.php">Unesi zapis</a></li>
+            <li><a href="admin/upit.php">Izvrši upit</a></li>
+            <li><a href="admin/upit-izlistava.php">Izlistaj upit</a></li>
+        </ul>
+
+        <p>Takođe se možeš i <a href="<?php echo ROOT_PATH;?>odjava.php">odjaviti</a>.</p>
+
+    </div>
+
+<?php include_once(ROOT_PATH . "ukljuci/podnozje.php"); ?>
--- prijava.php
+++ prijava.php
@@ -22,7 +22,7 @@
 
         if (!$ulogovan) { ?>
 
-            <p>Moraš biti prijavljen da bi pristupio administraciji. </p>
+            <p>Prijavi se da bi pristupio administraciji. </p>
 
             <form action="<?php $_SERVER[PHP_SELF]; ?>" method="post" id="formular">
 
@@ -37,23 +37,10 @@
 
     <?php
 
-        } else {    // ako jeste prijavljen prikazuje sve
-    ?>
-
-        <p>Prijavljen si i imaš pristup sledećim opcijama: </p>
-
-        <ul>
-            <li><a href="admin/provera.php">Pretraži i ažuriraj dokumente</a></li>
-            <li><a href="admin/taguje-sve.php">Masovno taguj</a></li>
-            <li><a href="admin/unesi-zapis.php">Unesi zapis</a></li>
-            <li><a href="admin/upit.php">Izvrši upit</a></li>
-            <li><a href="admin/upit-izlistava.php">Izlistaj upit</a></li>
-        </ul>
-
-        <p>Takođe se možeš i <a href='odjava.php'>odjaviti</a>.</p>
-
-    <?php
-        }    // kraj else
+        } else {    // ako jeste prijavljen
+          header("Location:" . ROOT_PATH  ."admin/index.php");
+          die();
+        }
     ?>
 
     </div>
