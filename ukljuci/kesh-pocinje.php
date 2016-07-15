9348bdf497cc8b6dc1b2d34bb044fc4b9277aba3
Damjan <dpavlica@itekako.com>
2016-07-15 13:48:56 +0200

    m

--- ukljuci/kesh-pocinje.php
+++ ukljuci/kesh-pocinje.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($ulogovan) {
+  die();
+}
+
 $kesh_ekstenzija = '.html';
 $kesh_trajanje = $kesh_trajanje ?: 3600;  // 1 sat = 3600 sek
 $kesh_folder = '.kesh/';
@@ -7,8 +11,7 @@ $original_url = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SE
 $keshiran_fajl = $kesh_folder.md5($original_url).$kesh_ekstenzija;
 
 ob_start('ob_gzhandler'); // počinje output buffering with gzip compression
-// ne servira keš ulogovanim korisnicima
-if (!$ulogovan && file_exists($keshiran_fajl) && time() - $kesh_trajanje < filemtime($keshiran_fajl)) { 
+if (!$ulogovan && file_exists($keshiran_fajl) && time() - $kesh_trajanje < filemtime($keshiran_fajl)) {
     readfile($keshiran_fajl); // čita keširan fajl
     echo '<!-- keširano '.date('d-m-Y \u H:i:s', filemtime($keshiran_fajl)).', stranica: '.$original_url.' -->';
     ob_end_flush(); // završava output buffering
