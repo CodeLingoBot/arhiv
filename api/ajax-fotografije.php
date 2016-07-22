a9c16c1a55cc61461ec42232bb26f2866b4f9697
Damjan <dpavlica@itekako.com>
2016-07-22 11:36:23 +0200

    fotografije za mobilni pojam (9)

--- api/ajax-fotografije.php
+++ api/ajax-fotografije.php
@@ -38,10 +38,8 @@ if ($broj_tagovanih_slika > 0) {
                 }
             } // for
         } // if
-        $izvor_slike = ROOT_PATH . "slike/smanjene/$br_slike-200px.jpg";
-        list($width, $height) = getimagesize($izvor_slike);
-        echo "Shirina slike je: " . $width;
-        $orjentacija_slike = ($width > $height) ? "polozena" : "uspravna";
+        $izvor_slike = BASE_URL . "slike/smanjene/$br_slike-200px.jpg";
+        $orjentacija_slike = jelPolozena($izvor_slike) ? "polozena" : "uspravna";
         echo "<a target='_blank' href='izvor.php?br=$br_slike&vrsta=3'><img class='slike $orjentacija_slike' src='$izvor_slike'></a>";
     } // for
     $tagovi_fotografija = json_encode($svi_tagovi);
