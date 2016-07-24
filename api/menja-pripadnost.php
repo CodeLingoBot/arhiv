d61e03dc3f72500af6221c41738bfbedaa11394d
mudroljub <mudroljub@gmail.com>
2016-07-24 02:35:56 +0200

    dokument izdali (11)

--- api/menja-pripadnost.php
+++ api/menja-pripadnost.php
@@ -1,13 +1,13 @@
 <?php
-
 require_once("../ukljuci/povezivanje2.php");
 
 $dokument_id = $_POST['broj_oznake'];
 $nova_pripadnost = $_POST['nova_pripadnost'];
-
 $nova_pripadnost = $mysqli->real_escape_string($nova_pripadnost);
-$mysqli->query("UPDATE dokumenti SET pripadnost='$nova_pripadnost' WHERE id=$dokument_id ;");
+
+$upit = "UPDATE dokumenti SET pripadnost='$nova_pripadnost' WHERE id=$dokument_id ;";
+$mysqli->query($upit);
 
 echo "<i>Promenjeno! </i>";
-echo "UPDATE dokumenti SET pripadnost='$nova_pripadnost' WHERE id=$dokument_id";
+echo $upit;
 ?>
