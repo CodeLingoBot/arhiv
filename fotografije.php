0b3ed14887c3aeb698bc4a7d68a97c29c045c74f
mudroljub <mudroljub@gmail.com>
2018-08-25 15:03:26 +0200

    sredjena galerija

--- fotografije.php
+++ fotografije.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $naslov = "Fotogalerija";
 require_once("ukljuci/config.php");
 include_once(ROOT_PATH . 'ukljuci/zaglavlje.php');
@@ -33,22 +32,21 @@ if ($trenutna_strana > $ukupno_stranica) {
                 <button class="izaberi">Prikaži</button>
             </p>
 
-            <?php
-                for($i = 1; $i <= $ukupno_stranica; $i++) {
-                    echo "<button type='submit' title='Fotografije strana $i'";
-                    if ($i == $trenutna_strana) {echo "class='trenutna_stranica'";}
-                    echo "name='stranica' value='$i'>$i</button>\n";
-                }
-            ?>
-
         <?php
+        for($i = 1; $i <= $ukupno_stranica; $i++) {
+            echo "<button type='submit' title='Fotografije strana $i'";
+            if ($i == $trenutna_strana) {echo "class='trenutna_stranica'";}
+            echo "name='stranica' value='$i'>$i</button>\n";
+        }
+
         $prikazuje_od = $slika_po_strani * ($trenutna_strana-1) + 1;
         $prikazuje_do = $slika_po_strani * $trenutna_strana;
         if ($prikazuje_do > $ukupno_fotografija) {
             $prikazuje_do = $ukupno_fotografija;
         }
 
-        echo "<p>Prikazuje fotografije od $prikazuje_od do $prikazuje_do: </p>\n";
+        echo "<p>Prikazuje fotografije od $prikazuje_od do $prikazuje_do: </p>
+        <div class='flex-between'>";
 
         for($j = 1; $j <= $ukupno_fotografija; $j++) {
             $red_za_fotke = $rezultat->fetch_assoc();
@@ -58,20 +56,20 @@ if ($trenutna_strana > $ukupno_stranica) {
 
             if($j >= $prikazuje_od && $j <= $prikazuje_do) {
                 $izvor_slike = REMOTE_ROOT . "slike/smanjene/$inv-200px.jpg";
-                echo "<div class='okvir-slike siva-ivica'><a href='fotografija.php?br=$inv'><img class='galerija-slike' src=$izvor_slike></a><br>";
+                echo "<div class='okvir-slike siva-ivica'>
+                    <a href='fotografija.php?br=$inv'><img class='galerija-slika' src=$izvor_slike></a><br>";
                 if($tekstualni_opis) {
                     echo "<div class='tekst-opis'>" . $tekstualni_opis . "</div>";
                 } else if($opis) {
-                    echo "<img class='opis-slike' src='http://znaci.net/o_slikama/$opis.jpg'>";
+                    echo "<div class='tekst-opis'>Otvori sliku za opis</div>";
                 }
                 echo "</div>\n";
-            } // if
-        } // for
-        ?>
+            }
+        }
+        echo "</div>";
 
-        <?php
-            $prethodna = $trenutna_strana - 1;
-            $naredna = $trenutna_strana + 1;
+        $prethodna = $trenutna_strana - 1;
+        $naredna = $trenutna_strana + 1;
         ?>
         <p>
             <button class="leva-strelica" name='stranica' value='<?php echo $prethodna; ?>'>&#8592;</button>
