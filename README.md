fb91f69843fba27af9053188fa3bcc2582d0d7d5
mudroljub <mudroljub@gmail.com>
2018-08-25 14:21:35 +0200

    galerija okvir slika sredjen

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ Podrazumevano keš traje 7 dana ali se pre uključenja zaglavlja može podesiti 
 Tutorijal za keširanje pročitaj [ovde](https://www.sanwebe.com/2013/09/php-cache-dynamic-pages-speed-up-load-times).
 
 ## TODO
-- srediti galeriju, kad kliknes uvecanje prikazuje thumb
+- dodati efekat na slike
 - na tri mesta dodaje "u oslobodilačkom ratu, pojam, klasa pojam, svi pojmovi"
   - napraviti metodu da se svi pojmovi vrste lokacija prikazuju "- u drugom svetskom ratu"
   - prebaciti Istra u oslobodilačkom ratu da bude lokacija, ne tema, i preimenovati u Istra
--- fotografije.php
+++ fotografije.php
@@ -22,20 +22,20 @@ if ($trenutna_strana > $ukupno_stranica) {
 ?>
 
     <div class="okvir">
-        Ukupno fotografija: <?php echo $ukupno_fotografija; ?>
+        <p>Ukupno fotografija: <?php echo $ukupno_fotografija; ?></p>
 
         <form action="<?php $_SERVER[PHP_SELF]; ?>" method="get">
-            <label for="slika_po_strani">Fotografija po stranici: </label>
-            <input type="number" name="slika_po_strani" value="<?php echo $slika_po_strani; ?>">
-            <label for="fraza">Fraza za pretragu: </label>
-            <input type="text" name="fraza" value="<?php echo $fraza; ?>">
-
-            <button class="izaberi">Prikaži</button>
-            <br>
+            <p>
+                <label for="slika_po_strani">Fotografija po stranici: </label>
+                <input type="number" name="slika_po_strani" value="<?php echo $slika_po_strani; ?>">
+                <label for="fraza">Fraza za pretragu: </label>
+                <input type="text" name="fraza" value="<?php echo $fraza; ?>">
+                <button class="izaberi">Prikaži</button>
+            </p>
 
             <?php
                 for($i = 1; $i <= $ukupno_stranica; $i++) {
-                    echo "<button type='submit'";
+                    echo "<button type='submit' title='Fotografije strana $i'";
                     if ($i == $trenutna_strana) {echo "class='trenutna_stranica'";}
                     echo "name='stranica' value='$i'>$i</button>\n";
                 }
@@ -48,7 +48,7 @@ if ($trenutna_strana > $ukupno_stranica) {
             $prikazuje_do = $ukupno_fotografija;
         }
 
-        echo "<p>Prikazujem fotografije od $prikazuje_od do $prikazuje_do: </p>\n";
+        echo "<p>Prikazuje fotografije od $prikazuje_od do $prikazuje_do: </p>\n";
 
         for($j = 1; $j <= $ukupno_fotografija; $j++) {
             $red_za_fotke = $rezultat->fetch_assoc();
@@ -59,27 +59,24 @@ if ($trenutna_strana > $ukupno_stranica) {
             if($j >= $prikazuje_od && $j <= $prikazuje_do) {
                 $izvor_slike = REMOTE_ROOT . "slike/smanjene/$inv-200px.jpg";
                 echo "<div class='okvir-slike siva-ivica'><a href='fotografija.php?br=$inv'><img class='galerija-slike' src=$izvor_slike></a><br>";
-                if($opis) {
-                    echo "<img class='opis-slike' src='http://znaci.net/o_slikama/$opis.jpg'>";
-                } else if($tekstualni_opis) {
+                if($tekstualni_opis) {
                     echo "<div class='tekst-opis'>" . $tekstualni_opis . "</div>";
-                } else {
-                    echo "<img class='opis-slike' src='slike/bez-opisa.jpg'>";
+                } else if($opis) {
+                    echo "<img class='opis-slike' src='http://znaci.net/o_slikama/$opis.jpg'>";
                 }
                 echo "</div>\n";
             } // if
         } // for
         ?>
 
-        <br>
-
         <?php
             $prethodna = $trenutna_strana - 1;
             $naredna = $trenutna_strana + 1;
         ?>
-
+        <p>
             <button class="leva-strelica" name='stranica' value='<?php echo $prethodna; ?>'>&#8592;</button>
             <button class="desna-strelica" name='stranica' value='<?php echo $naredna; ?>'>&#8594;</button>
+        </p>
         </form>
     </div>
 
